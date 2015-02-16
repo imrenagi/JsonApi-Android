b1e4393f008d4d157cdb06d50245ca0ccc6e0ccd
Imre Nagi <imre@icehousecorp.com>
2015-02-16 18:51:21 +0700

    Add parent class for a resource object. A resource object class must extend this class to get id, href, and type property.

---
+++ jsonapi/src/main/java/com/icehousecorp/jsonapi/JSONAPIResourceObject.java
@@ -0,0 +1,13 @@
+package com.icehousecorp.jsonapi;
+
+import java.io.Serializable;
+
+/**
+ * Created by imrenagi on 2/16/15.
+ */
+public class JSONAPIResourceObject implements Serializable {
+    
+    private String id;
+    private String type;
+    private String href;
+}
